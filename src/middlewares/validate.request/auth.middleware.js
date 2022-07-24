d2cc07a316818b7db8720b44c550259a98e3d472
Isabela de Aro <euisabeladearo@gmail.com>
2022-07-24 07:25:42 -0300

    feat: add authentication to swagger routers

--- src/middlewares/validate.request/auth.middleware.js
+++ src/middlewares/validate.request/auth.middleware.js
@@ -1,12 +1,19 @@
 const { StatusCodes } = require('http-status-codes');
 const { authenticateToken } = require('../../helpers/JWTToken');
 
+const checkBearerToken = (token) => {
+  const bearerToken = /\s/g.test(token);
+  if (bearerToken) return token.split(' ')[1];
+  return token;
+};
+
 module.exports = async (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = await checkBearerToken(req.headers.authorization);
 
   if (!token) return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Token não encontrado' });
 
   const payload = await authenticateToken(token);
+  console.log(payload);
 
   if (!payload) return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Token expirado ou inválido' });
 
